7931c68930f0d8ad7c94db78e39eb265faaeaaf6
Andrea Arighi <andrea@chino.io>
2019-03-07 10:06:57 +0100

    Restore missing files
    Files were removed by accident during last release, together with obj and bin folders.
    
    Fix issue #1

---
+++ Chino/ChinoApiException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Chino
+{
+    class ChinoApiException : Exception
+    {
+        public ChinoApiException(String message) : base(message)
+        {
+            
+        }
+    }
+}
