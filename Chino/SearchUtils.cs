23d79372bff7e2bfee72575e0ad98a463347f223
Andrea Arighi <andrea@chino.io>
2019-04-15 12:18:19 +0200

    Add SortRule enum and class

--- Chino/SearchUtils.cs
+++ Chino/SearchUtils.cs
@@ -98,5 +98,65 @@ namespace Chino
         StringBuilder getString();
         string parseJson(int indentLevel);
     }
+
+
+
+    public enum OrderEnum
+    {
+        /* ascending order */
+        ASC,
+
+        /* descending order */
+        DESC
+    }
+
+    public class Order
+    {
+        private readonly OrderEnum _enumValue;
+
+        public Order(OrderEnum enumValue)
+        {
+            _enumValue = enumValue;
+        }
+
+        public string toString() {
+            return _enumValue.ToString().ToLower();
+        }
+    }
+
+    public class SortRule {
+
+        private string fieldName;
+        private Order order;
+
+        SortRule(string field, Order order) {
+            fieldName = field;
+            this.order = order;
+        }
+
+        SortRule(string field, OrderEnum order) 
+            : this(field, new Order(order)) {}
+
+        public StringBuilder toJSONString(int indentLevel) {
+            var sb = indent(new StringBuilder(), indentLevel).Append("{\n");
+            indent(sb, indentLevel)
+                .Append("\t")
+                .Append("\"field\": ").Append("\"").Append(fieldName).Append("\",\n");
+            indent(sb, indentLevel)
+                .Append("\t")
+                .Append("\"order\": ").Append("\"").Append(order.ToString()).Append("\"\n");
+            indent(sb, indentLevel)
+                .Append("}");
+
+            return sb;
+        }
+
+        private static StringBuilder indent(StringBuilder sb, int level) {
+            for(var i=0; i<level; i++) {
+                sb.Append("\t");
+            }
+            return sb;
+        }
+    }
     
 }
