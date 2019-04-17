4224966982ff42e4dd2a1989a505692a267172e3
Andrea Arighi <andrea@chino.io>
2019-04-17 10:38:46 +0200

    Fix test execution
    - Ask user to set 'automated_test=allow'
    - deleteAll() now works with offset, thus eliminating every object on
    the account, instead of only calling list() once

--- ChinoTest/Const.cs
+++ ChinoTest/Const.cs
@@ -11,6 +11,7 @@ namespace ChinoTest {
         public static string _customerId;
         public static string _customerKey;
         public static string _hostUrl;
+        public static bool TestAllowed = false;
 
         internal static bool isInitialized()
         {
@@ -19,39 +20,86 @@ namespace ChinoTest {
 
         public static void deleteAll(ChinoAPI chino)
         {
+            if (!TestAllowed)
+            {
+                Console.Error.WriteLine();
+                Console.Error.WriteLine("WARNING: running tests will delete everything on the Chino.io account! " +
+                                        "If you still want to run the tests, set in your environment variables 'automated_test=allow' and re-run the suite." +
+                                        "\n");
+                throw new ApplicationException(
+                    "WARNING: running tests will delete everything on the Chino.io account! " +
+                    "If you still want to run the tests, set in your environment variables 'automated_test=allow' and re-run the suite." +
+                    "\n"
+                );
+            }
+            // delete Documents / Schemas / Repos
             List<Repository> repos = chino.repositories.list(0).repositories;
-            foreach (Repository r in repos) {
-                List<Schema> schemas = chino.schemas.list(r.repository_id, 0).schemas;
-                foreach (Schema s in schemas)
-                {
-                    List<Document> documents = chino.documents.list(s.schema_id, 0).documents;
-                    foreach (Document d in documents)
+            var repos_offset = 0;
+            while (repos.Count > 0)
+            {
+                foreach (Repository r in repos) {
+                    List<Schema> schemas = chino.schemas.list(r.repository_id, 0).schemas;
+                    var schemas_offset = 0;
+                    while (schemas.Count > 0)
                     {
-                        Console.WriteLine(chino.documents.delete(d.document_id, true));
+                        foreach (Schema s in schemas)
+                        {
+                            List<Document> documents = chino.documents.list(s.schema_id, 0).documents;
+                            var docs_offset = 0;
+                            while (documents.Count > 0)
+                            {
+                                foreach (Document d in documents)
+                                {
+                                    Console.WriteLine(chino.documents.delete(d.document_id, true));
+                                }
+                                docs_offset += documents.Count;
+                                documents = chino.documents.list(s.schema_id, docs_offset).documents;
+                            }
+                            Console.WriteLine(chino.schemas.delete(s.schema_id, true));
+                        }
+                        schemas_offset += schemas.Count;
+                        schemas = chino.schemas.list(r.repository_id, schemas_offset).schemas;
                     }
-                    Console.WriteLine(chino.schemas.delete(s.schema_id, true));
+                    Console.WriteLine(chino.repositories.delete(r.repository_id, true));
                 }
-                Console.WriteLine(chino.repositories.delete(r.repository_id, true));
+                repos_offset += repos.Count;
+                repos = chino.repositories.list(repos_offset).repositories;
             }
+            // delete UserSchemas (and Users by consequence)
             List<UserSchema> userSchemas = chino.userSchemas.list(0).user_schemas;
-            foreach (UserSchema u in userSchemas)
+            var userSchemas_offset = 0;
+            while (userSchemas.Count > 0)
             {
-                List<User> users = chino.users.list(u.user_schema_id, 0).users;
-                foreach (User us in users)
+                foreach (UserSchema u in userSchemas)
                 {
-                    Console.WriteLine(chino.users.delete(us.user_id, true));
+                    Console.WriteLine(chino.userSchemas.delete(u.user_schema_id, true));
                 }
-                Console.WriteLine(chino.userSchemas.delete(u.user_schema_id, true));
+                userSchemas_offset += userSchemas.Count;
+                userSchemas = chino.userSchemas.list(userSchemas_offset).user_schemas;
             }
+            // delete Collections
             List<Collection> collections = chino.collections.list(0).collections;
-            foreach (Collection c in collections)
+            var coll_offset = 0;
+            while (collections.Count > 0)
             {
-                Console.WriteLine(chino.collections.delete(c.collection_id, true));
+                foreach (Collection c in collections)
+                {
+                    Console.WriteLine(chino.collections.delete(c.collection_id, true));
+                }
+                coll_offset += collections.Count;
+                collections = chino.collections.list(coll_offset).collections;
             }
+            // delete Groups
             List<Group> groups = chino.groups.list(0).groups;
-            foreach (Group g in groups)
+            var groups_offset = 0;
+            while (groups.Count > 0)
             {
-                Console.WriteLine(chino.groups.delete(g.group_id, true));
+                foreach (Group g in groups)
+                {
+                    Console.WriteLine(chino.groups.delete(g.group_id, true));
+                }
+                groups_offset += groups.Count;
+                groups = chino.groups.list(0).groups;
             }
         }
     }
